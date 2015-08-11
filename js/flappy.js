ad241b6592a53fb2164b7cc813b0a975b4218291
V3513 (Conference ID) <v3513@cam.ac.uk>
2015-08-11 17:00:24 +0100

    day 2

--- js/flappy.js
+++ js/flappy.js
@@ -1,117 +1,125 @@
-// the Game object used by the phaser.io library
-var stateActions = { preload: preload, create: create, update: update };
-
-// Phaser parameters:
-// - game width
-// - game height
-// - renderer (go for Phaser.AUTO)
-// - element where the game will be drawn ('game')
-// - actions on the game state (or null for nothing)
-var game = new Phaser.Game(790, 400, Phaser.AUTO, 'game', stateActions);
-
-/*
- * Loads all resources for the game and gives them names.
+/**
+ * Created by v3513 on 11/08/2015.
  */
+// the functions associated with preload, create and update.
+var actions = { preload: preload, create: create, update: update };
+// the Game object used by the phaser.io library
+var game = new Phaser.Game(790, 400, Phaser.AUTO, "game", actions);
+// Global score variable initialised to 0.
 var score = 0;
+// Global variable to hold the text displaying the score.
 var labelScore;
+// Global player variable declared but not initialised.
 var player;
+// Global pipes variable initialised to the empty array.
 var pipes = [];
+// the interval (in seconds) at which new pipe columns are spawned
+var pipeInterval = 1.75;
+
+// Loads all resources for the game and gives them names.
+jQuery("#greeting-form").on("submit", function(event_details) {
+    var greeting = "Hello ";
+    var name = jQuery("#fullName").val();
+    var greeting_message = greeting + name;
+    jQuery("#greeting-form").hide();
+    jQuery("#greeting").append("<p>" + greeting_message + "</p>");
+    event_details.preventDefault();
+
+});
+
 
 function preload() {
-    game.load.image("playerIMG", "../assets/burger2.gif");
-game.load.audio("score", "../assets/point.ogg");
-game.load.image("pipe","../assets/pipe.png");
+    // make image file available to game and associate with alias playerImg
+    game.load.image("playerIMG","../assets/burger2.gif");
+    // make sound file available to game and associate with alias score
+    game.load.audio("score", "../assets/point.ogg");
+    // make image file available to game and associate with alias pipe
+    game.load.image("pipe","../assets/pipe.png");
+    game.load.image("background", "../assets/frenchfriesbackground.jpg");
 }
 
-/*
- * Initialises the game. This function is only called once.
- */
+// Initialises the game. This function is only called once.
 function create() {
     // set the background colour of the scene
-    game.stage.setBackgroundColor("#99CCFF");
-    game.add.text(250, 20, "Welcome to my game", {font: "30px Segoe UI Semibold", fill: "#FFFFFF"});
+    game.stage.setBackgroundColor("#F3D3A3");
+    game.add.sprite(0,0, "background");
+    // add welcome text
 
-        var burger = game.add.sprite(10, 270, "playerIMG");
-        burger.scale.setTo(0.05, 0.05);
+    // add score text
+    labelScore = game.add.text(20, 0, "0",
+        {font: "30px Arial", fill: "#FFFFFF"});
+    // initialise the player and associate it with playerImg
 
-
-        var burger = game.add.sprite(10, 40, "playerIMG");
-        burger.scale.setTo(0.05, 0.05);
-
-
-        var burger = game.add.sprite(700, 40, "playerIMG");
-        burger.scale.setTo(0.05, 0.05);
-
-        var burger = game.add.sprite(700, 270, "playerIMG");
-        burger.scale.setTo(0.05, 0.05);
-
-
-    game.input
-            .onDown
-            .add(clickHandler);
-    game.input
-        .keyboard.addKey(Phaser.Keyboard.SPACEBAR)
-        .onDown.add(spaceHandler);
-    alert(score);
-    generatePipe();
-
-labelScore = game.add.text(20, 20, "0");
-
-   var burger = player = game.add.sprite(100, 200, "playerIMG");
+    var burger = player = game.add.sprite(80, 200, "playerIMG");
     burger.scale.setTo(0.05, 0.05);
-    game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
-        .onDown.add(moveRight);
-    game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
-        .onDown.add(moveLeft);
-    game.input.keyboard.addKey(Phaser.Keyboard.UP)
-        .onDown.add(moveUp);
-    game.input.keyboard.addKey(Phaser.Keyboard.DOWN)
-        .onDown.add(moveDown);
-}
-function clickHandler(event){
-    alert("click!");
-    alert("The position is: " + event.x + "," + event.y);
-    var burger = game.add.sprite(event.x, event.y, "playerIMG");
-    burger.scale.setTo(0.05, 0.05);
-}
-function spaceHandler(){
-  game.sound.play("score");
+    // Start the ARCADE physics engine.
+    // ARCADE is the most basic physics engine in Phaser.
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    // enable physics for the player sprite
+    game.physics.arcade.enable(player);
+    // set the player's gravity
+    player.body.gravity.y = 500;
+    // associate spacebar with jump function
+    game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(playerJump);
+    // time loop for game to update
+    game.time.events.loop(pipeInterval * Phaser.Timer.SECOND, generatePipe);
 }
 
-function changeScore() {
-    score = score + 1;
-    labelScore.setText(score.toString());
-}
-function moveRight() {
-    player.x = player.x + 10
-}
-function moveLeft() {
-    player.x = player.x - 10
-}
-function moveUp() {
-    player.y = player.y - 10
-}
-function moveDown() {
-    player.y = player.y + 10
+// This function updates the scene. It is called for every new frame.
+function update() {
+    // Call gameOver function when player overlaps with any pipe
+
+    game.physics.arcade
+        .overlap(player,
+        pipes,
+        gameOver);
+
 }
 
+// Adds a pipe part to the pipes array
+function addPipeBlock(x, y) {
+    // make a new pipe block
+    var block = game.add.sprite(x,y,"pipe");
+    // insert it in the pipe array
+    pipes.push(block);
+    // enable physics engine for the block
+    game.physics.arcade.enable(block);
+    // set the block's horizontal velocity to a negative value
+    // (negative x value for velocity means movement will be towards left)
+    block.body.velocity.x = -200;
+}
+
+// Generate moving pipe
 function generatePipe() {
-        var gapStart = game.rnd.integerInRange(1, 5);
-        for (var count=0; count<8; count=count+1) {
-            if(count != gapStart && count != gapStart + 1) {
-                addPipeBlock(40, count*50);
-            }
+    // Generate  random integer between 1 and 5. This is the location of the
+    // start point of the gap.
+    var gapStart = game.rnd.integerInRange(1, 5);
+    // Loop 8 times (8 is the height of the canvas).
+    for (var count = 0; count < 8; count++) {
+        // If the value of count is not equal to the gap start point
+        // or end point, add the pipe image.
+        if(count != gapStart && count != gapStart+1){
+            addPipeBlock(750, count * 50);
         }
     }
-function addPipeBlock(x, y) {
-    // create a new pipe block
-    var block = game.add.sprite(x,y,"pipe");
-    // insert it in the 'pipes' array
-    pipes.push(block);
+    // Increment the score each time a new pipe is generated.
+    changeScore();
 }
 
-/*
- * This function updates the scene. It is called for every new frame.
- */
-function update() {
+function playerJump() {
+    // the more negative the value the higher it jumps
+    player.body.velocity.y = -200;
+}
+
+// Function to change the score
+function changeScore() {
+    //increments global score variable by 1
+    score++;
+    // updates the score label
+    labelScore.setText(score.toString());
+}
+
+function gameOver() {
+    // stop the game (update() function no longer called)
+    game.destroy();
 }
