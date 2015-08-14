986fb0713ef782723a40de8e39db6023ad54b394
V3513 (Conference ID) <v3513@cam.ac.uk>
2015-08-14 16:02:22 +0100

    day 3

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.post('/score', function(request, response){
     
 });
 
-var server = app.listen(8080, function() {
+var server = app.listen((process.env.PORT || 8080), function() {
     var host = server.address().address;
     var port = server.address().port;
 
