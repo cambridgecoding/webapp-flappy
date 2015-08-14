f342b1fdece641cb3217f0d401119b8a9cc06683
vbissett <victoria.bissett@hotmail.com>
2015-08-14 12:07:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
+var csv = require('ya-csv');
 
 var app = express();
 app.use(express.static(path.join(__dirname, "")));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(request, response){
     response.sendFile(path.join(__dirname, "pages/index.html"));
 });
 
 app.post('/score', function(request, response){
-    
+    var name = request.body.fullName;
+    var email = request.body.email;
+    var score = request.body.score;
+
+    var database = csv.createCsvFileWriter("scores.csv", {"flags": "a"});
+    var data = [name, email, score];
+
+    database.writeRecord(data);
+    database.writeStream.end();
+
+    response.redirect("/");
+});
+
+app.get("/score", function(request, response) {
+    var reader = csv.createCsvFileReader("scores.csv");
+    reader.setColumnNames(['name', 'email', 'score']);
+
+    var scores = [];
+    reader.addListener('data', function(data) {
+        scores.push(data);
+    });
+
+    reader.addListener('end', function(){
+        response.send(scores);
+    })
 });
 
 var server = app.listen(8080, function() {
@@ -18,3 +45,6 @@ var server = app.listen(8080, function() {
 
     console.log("Bob's Flappy Bird listening at http://%s:%s", host, port);
 });
+
+
+
