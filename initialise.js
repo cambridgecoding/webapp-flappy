5a2f6a710b32999607da7464b129f587d9520e81
Raphaël Proust <raphlalou@gmail.com>
2016-07-01 10:56:35 +0100

    Adding initialise script

---
+++ initialise.js
@@ -0,0 +1,10 @@
+var exec = require('child_process').exec;
+function printer(error, stdout, stderr) {
+  if(error){
+    console.error('exec error: ' + error);
+  }
+  console.log(stdout);
+  console.warn(stderr);
+}
+exec("apm install git-plus", printer);
+
