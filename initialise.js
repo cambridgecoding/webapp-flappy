cf3108692e2cd6840f80e896c803988080697d64
raoulDoc <raoul@urma.com>
2016-07-04 12:00:43 +0100

    Create initialise.js

---
+++ initialise.js
@@ -0,0 +1,10 @@
+var exec = require('child_process').exec;
+function printer(error, stdout, stderr) {
+  if(error){
+    console.error('exec error: ' + error);
+  }
+  console.log(stdout);
+  console.warn(stderr);
+}
+exec("apm install atom-live-server", printer);
+exec("apm install linter-jshint", printer);
