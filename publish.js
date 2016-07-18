b55ba9e4a5e1ce39c18a02d33a6d35390eede97f
raoulDoc <raoul@urma.com>
2016-07-18 12:34:07 +0100

    Update publish.js to Add/Commit/Push in one go

--- publish.js
+++ publish.js
@@ -6,4 +6,15 @@ function printer(error, stdout, stderr) {
   console.log(stdout);
   console.warn(stderr);
 }
+
+if (!Date.now) {
+  Date.now = function now() {
+    return new Date().getTime();
+  };
+}
+
+console.log("Commiting code locally");
+exec("git add .", printer);
+exec("git commit -a -m \"latest update \"" + Date.now(), printer);
+console.log("Pusing code to github");
 exec("git push origin master:gh-pages --force", printer);
